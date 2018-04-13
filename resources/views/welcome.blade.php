c77a1827f8e61570c59fb21fac6b0f9714b1b8e1
linxcodev <fajrul452@gmail.com>
2018-04-13 09:16:57 +0700

    learn perpustakaan and change asset

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+{{-- <!doctype html> --}}
+@extends('layouts.app')
+
+@section('content')
+  {{-- Perpustakaan --}}
+  <div class="container">
+      <div class="row justify-content-center">
+          <div class="col-md-8">
+              <div class="card">
+                  <div class="card-header bg-primary text-white">Perpustakaan Online</div>
+
+                  <div class="card-body">
+                      Selamat Datang Di Perpustakaan Online
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  @endsection

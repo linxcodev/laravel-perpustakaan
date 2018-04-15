a7c78436a2ac84d8e83f3abbd0e4e819d920e30f
linxcodev <fajrul452@gmail.com>
2018-04-15 08:30:35 +0700

    konfigurasi register

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+      // unutk memanggil Seeder yang banyak ke database tambahkan syntak dibawah ini
+        $this->call(UsersTableSeeder::class);
     }
 }

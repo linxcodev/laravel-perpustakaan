6f544fca021bc3b310eb9461e50db380fbdd460e
linxcodev <fajrul452@gmail.com>
2018-04-17 10:41:36 +0700

    learn partial view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,16 +82,9 @@
         </nav>
 
         <main class="py-4">
-          @if (session()->has('flash_notification.message'))
-            <div class="container">
-              <div class="alert alert-{{ session()->get('flash_notification.level') }}">
-                <button type="button" class="close" data-dismiss="alert"
-                aria-hidden="true">&times;</button>
-                {!! session()->get('flash_notification.message') !!}
-              </div>
-            </div>
+          {{-- @include('layouts._messages') --}}
+          @include('layouts._messages')
 
-          @endif
             @yield('content')
         </main>
     </div>

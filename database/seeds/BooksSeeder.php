cfff2edd083a7c66de7476c50c9b27bb300f61cf
linxcodev <fajrul452@gmail.com>
2018-04-20 16:44:57 +0700

    create memberBorrow until migration with seeder

--- database/seeds/BooksSeeder.php
+++ database/seeds/BooksSeeder.php
@@ -1,6 +1,8 @@
 <?php
 use App\Book;
+use App\User;
 use App\Author;
+use App\BorrowLog;
 use Illuminate\Database\Seeder;
 
 class BooksSeeder extends Seeder
@@ -23,16 +25,36 @@ class BooksSeeder extends Seeder
           'author_id' => $author1->id,
         ]);
 
-        $book1 = Book::create([
+        $book2 = Book::create([
           'title' => 'Bait-bait puisi1',
           'amount' => 5,
           'author_id' => $author2->id,
         ]);
 
-        $book1 = Book::create([
+        $book3 = Book::create([
           'title' => 'Bait-bait puisi2',
           'amount' => 7,
           'author_id' => $author3->id,
+        ]);
+
+        $member = User::where('email', 'member@mail.com')->first();
+
+        BorrowLog::create([
+          'user_id' => $member->id,
+          'book_id' => $book1->id,
+          'is_returned' => 0
+        ]);
+
+        BorrowLog::create([
+          'user_id' => $member->id,
+          'book_id' => $book2->id,
+          'is_returned' => 0
+        ]);
+
+        BorrowLog::create([
+          'user_id' => $member->id,
+          'book_id' => $book3->id,
+          'is_returned' => 1
         ]);
 
     }

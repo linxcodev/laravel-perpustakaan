2e9b775bc83aeb1266defe1043008c4c8643fc60
linxcodev <fajrul452@gmail.com>
2018-04-28 16:29:08 +0700

    add nav active

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,24 +41,24 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         @if (auth()->check())
-                          <li>
+                          <li class="nav-item {{ Route::is('home') ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('home') }}">Beranda</a>
                           </li>
                           @role('admin')
-                          <li>
+                          <li class="nav-item {{ Route::is('authors.index') ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('authors.index') }}">Penulis</a>
                           </li>
-                          <li>
+                          <li class="nav-item {{ Route::is('books.index') ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('books.index') }}">Buku</a>
                           </li>
-                          <li>
+                          <li class="nav-item {{ Route::is('members.index') ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('members.index') }}">Member</a>
                           </li>
-                          <li>
+                          <li class="nav-item {{ Route::is('statistics.index') ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('statistics.index') }}">Data Peminjaman</a>
                           </li>
                           @endrole
-                          <li class="nav-item">
+                          <li class="nav-item {{ Route::is('profile') ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('profile') }}">Profile</a>
                           </li>
 

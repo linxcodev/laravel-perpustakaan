a9a49d166af0a189ed789f64419e0dbac5ad2a8b
linxcodev <fajrul452@gmail.com>
2018-04-29 10:28:35 +0700

    Ecport data to excel

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -13,7 +13,8 @@
               </nav>
               <div class="card">
                   <div class="card-header">Buku
-                    <a class="btn btn-primary float-right" href="{{ route('books.create') }}">Tambah</a>
+                    <a class="btn btn-success float-right" href="{{ route('export.books.xls') }}">Export</a>
+                    <a class="btn btn-primary float-right" style=" margin-right: 10px" href="{{ route('books.create') }}">Tambah</a>
                   </div>
 
                   <div class="card-body">
